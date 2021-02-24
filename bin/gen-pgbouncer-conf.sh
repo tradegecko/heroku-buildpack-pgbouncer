96700243b1147db571da57503ad1fdc283a11902
Ezra Lim <ezra.lim@tradegecko.com>
2021-02-24 11:26:10 +0800

    Allow setting PGBOUNCER_SSLMODE to control server_tls_sslmode

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -3,6 +3,7 @@
 POSTGRES_URLS=${PGBOUNCER_URLS:-DATABASE_URL}
 POOL_MODE=${PGBOUNCER_POOL_MODE:-transaction}
 SERVER_RESET_QUERY=${PGBOUNCER_SERVER_RESET_QUERY}
+SSLMODE=${PGBOUNCER_SSLMODE:-prefer}
 n=1
 
 # if the SERVER_RESET_QUERY and pool mode is session, pgbouncer recommends DISCARD ALL be the default
@@ -17,7 +18,7 @@ listen_addr = 127.0.0.1
 listen_port = 6000
 auth_type = md5
 auth_file = /app/vendor/pgbouncer/users.txt
-server_tls_sslmode = prefer
+server_tls_sslmode = ${SSLMODE}
 server_tls_protocols = secure
 server_tls_ciphers = HIGH:!ADH:!AECDH:!LOW:!EXP:!MD5:!3DES:!SRP:!PSK:@STRENGTH
 
